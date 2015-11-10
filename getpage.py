19e73e79b01892e46e4a4a1f140aa89462070f29
Sean Chu <StridentStorm@users.noreply.github.com>
2015-11-10 12:45:46 -0500

    Added Key

--- getpage.py
+++ getpage.py
@@ -1,13 +1,13 @@
 import urllib2
 import json
-
+key = "AIzaSyAbcq5DqVguTNomXJhuQhLp2ZX_0q4pOXY"
 def returnTitles(searchterm):
     """
     Returns titles of books related to search term given
     Params: String searchterm
     """
 
-    url = "https://www.googleapis.com/books/v1/volumes?q=" + searchterm
+    url = "https://www.googleapis.com/books/v1/volumes?q=" + searchterm + "&key=" + key
     request = urllib2.urlopen(url)
     result = request.read()
     r = json.loads(result)
