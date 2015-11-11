433c1e6718242735ad8279812a0dee7bcf6da6d6
TiffL <lei.tiffany@gmail.com>
2015-11-10 20:36:58 -0500

    generalized returnTitles

--- getpage.py
+++ getpage.py
@@ -1,19 +1,27 @@
 import urllib2
 import json
-key = "AIzaSyAbcq5DqVguTNomXJhuQhLp2ZX_0q4pOXY"
-def returnTitles(searchterm):
-    """
-    Returns titles of books related to search term given
-    Params: String searchterm
-    """
 
+key = "AIzaSyAbcq5DqVguTNomXJhuQhLp2ZX_0q4pOXY"
+def returnBooksInfo(infoItem,searchterm):
+    """
+    Returns specified information for books related to search term given
+    Params: String infoItem (ex.: "title"), String searchterm
+    """
+    info = ""
     url = "https://www.googleapis.com/books/v1/volumes?q=" + searchterm + "&key=" + key
     request = urllib2.urlopen(url)
     result = request.read()
     r = json.loads(result)
 
     for i in r['items']:
-        print i['volumeInfo']['title']
+        if infoItem == "authors":
+            for j in i['volumeInfo'][infoItem]:
+                info += "\n" + j
+        else:
+            info += "\n" + i['volumeInfo'][infoItem]
+
+    return info
     
 #Testing
-returnTitles("Happiness")
+print returnBooksInfo("title","Happiness")
+print returnBooksInfo("authors","Happiness")
