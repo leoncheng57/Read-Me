89667c86ff44fae14d618d70f19d0990cb810536
leoncheng57 <57leonardo@gmail.com>
2015-11-10 23:52:58 -0500

    edits

--- test.py
+++ test.py
@@ -11,11 +11,13 @@ def imageTags(url):
 def imageProbs(url):
     return clarifai_api.tag_image_urls(url)["results"][0]["result"]["tag"]["probs"]
 
-#Testing
+
+
+#---------------------TESTING--------------------------
 
 #url1 = "http://www.clarifai.com/img/metro-north.jpg"
-#url1 = "https://static1.squarespace.com/static/5436dcd5e4b05ef245bb78c5/5436e746e4b05fd6079dfcf3/54d9cd14e4b079af934ddbf9/1430947393199/?format=1500w"
-url1 = "http://images.fastcompany.com/upload/happiness_bulldogdrummond.jpg"
+url1 = "https://static1.squarespace.com/static/5436dcd5e4b05ef245bb78c5/5436e746e4b05fd6079dfcf3/54d9cd14e4b079af934ddbf9/1430947393199/?format=1500w"
+#url1 = "http://images.fastcompany.com/upload/happiness_bulldogdrummond.jpg"
 
 print url1
 print
@@ -24,3 +26,4 @@ print
 print imageTags(url1)
 print
 print imageProbs(url1)
+
