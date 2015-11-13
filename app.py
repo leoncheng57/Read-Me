63ca8b32846e3e0c3b8d2edc2727570863c0de06
leoncheng57 <57leonardo@gmail.com>
2015-11-12 20:45:17 -0500

    working on keywords in results.html

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
-import getpage
+from flask import Flask, render_template, request, session, redirect, url_for
+import getpage, image
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
+@app.route("/home",methods=["GET","POST"])
 def home():
     if request.method=="GET":
         return render_template("home.html")
     else:
-        imageURL = request.form['imageURL']
+        session["img_url"] = request.form['img_url']
         return redirect(url_for("results"))
 
 @app.route("/results")
 def results():
-    keyword = ""
-    #test.py recieves imageURL + returns keyword
+    #keywords = []
+    print session["img_url"]
+    keywords = image.get_data(session["img_url"])
+    print keywords
+    #image.py recieves imageURL + returns keyword
     #getpage.py receives keyword + returns book information
-    info = getpage.returnBooksInfo("title",keyword)
-    return render_template("results.html",keyword=keyword,info=info)
+    #info=""
+    #info = getpage.returnBooksInfo("title",keyword)
+    #return render_template("results.html",keywords=keywords,info=info)
+    
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key = ""
     app.run(host='0.0.0.0',port=8000)
